8c37941f6ae0fcdbb6abbb1b133dfc7ce3eb3bfa
sprajjwal <sprajjwal@ucdavis.edu>
2019-08-30 13:07:34 -0700

    [ADD] added randomization

--- madlibs.py
+++ madlibs.py
@@ -2,8 +2,10 @@ import os
 from random import randrange
 
 #Function to randomize the output POS
-def random_output(pos_list):
-    return randrange(len(pos_list))
+def random_pos_output(pos_list):
+    rand = pos_list[randrange(0, len(pos_list))]
+    pos_list.pop(pos_list.index(rand))
+    return rand
 
 #Helper function to validate user input
 def user_input(pos, index):
@@ -23,10 +25,9 @@ def pos_input(pos, pos_dict):
 pos_dict = {}
 
 #String holding POS that user would input
-parts_of_speech = ["Adjective", "Adjective", "Adjective", "Adjective",
-"Adjective", "Adjective", "Adjective", "Animal", "Noun",
-"Noun", "Noun", "Part of the body", "Past tense verb",
-"Plural noun", "Type of Container"]
+parts_of_speech = ["Adjective", "Adjective", "Funny Noise", "Funny Noise",
+"Noun", "Noun", "Noun", "Noun", "Part of Body", "Place",
+"Plural Noun", "Plural Noun"]
 
 #Welcome screen
 print("--------------   WELCOME TO MADLIBS   --------------\n\n\n")
@@ -37,38 +38,30 @@ for pos in parts_of_speech:
         pass
     else:
         pos_dict[pos] = []
-
     pos_input(pos, pos_dict)
 
 
 #String with our madlib story
-story = '''Old Mother Hubbard went to the %s 
-to get her %s %s a bone. 
-When she got there, the %s was %s, 
-And so her %s dog had none.
-    
-Jack and Jill went up the %s 
-to fetch a %s of water. 
-Jack fell down and broke his %s 
-and Jill came tumbling after. 
-
-There was a girl, and she had a little curl 
-Right in the middle of her %s. 
-And when she was %s, she was very, very %s, 
-And when she was bad, she was %s.
-
-There was an %s woman 
-Who %s in a shoe. 
-She had so many %s, 
-She didnt know what to do.'''
+story = '''Almost every community in America now has a bowling %s
+because bowling has become very %s with young
+%s. Most of them become very %s
+at the game. The main object of the game is to roll a heavy bowling
+%s.down the alley and knock down the 12 pins
+which are at the other end. If you knock them down in one roll, it's
+called a %s. If it takes two rolls, it's called
+a %s. Many alleys have automatic %s
+setters. Others hire %s who set the pins by %s.
+The most important thing to remember when bowling is to make
+sure you have a good grip on the %s.or you're liable to
+drop it on your %s!'''
 
 #Clears the input page
 os.system('clear')
 
 #Printing the final story
-print(story %(pos_dict["Noun"][0], pos_dict["Adjective"][0], pos_dict["Animal"][0], 
-pos_dict["Noun"][0], pos_dict["Adjective"][1], pos_dict["Adjective"][2], 
-pos_dict["Noun"][1], pos_dict["Type of Container"][0], pos_dict["Noun"][2], 
-pos_dict["Part of the body"][0], pos_dict["Adjective"][3], pos_dict["Adjective"][4], 
-pos_dict["Adjective"][5], pos_dict["Adjective"][6], pos_dict["Past tense verb"][0], 
-pos_dict["Plural noun"][0]))
+print(story %(random_pos_output(pos_dict["Place"]), random_pos_output(pos_dict["Adjective"]),
+random_pos_output(pos_dict["Plural Noun"]), random_pos_output(pos_dict["Adjective"]),
+random_pos_output(pos_dict["Noun"]), random_pos_output(pos_dict["Funny Noise"]),
+random_pos_output(pos_dict["Funny Noise"]), random_pos_output(pos_dict["Noun"]),
+random_pos_output(pos_dict["Plural Noun"]), random_pos_output(pos_dict["Noun"]), 
+random_pos_output(pos_dict["Noun"]), random_pos_output(pos_dict["Part of Body"]) ))
